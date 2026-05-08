4067938eab5b629d381c27e1e1906a15acbec7b3
LDK2026 <tspmi@hotmail.com>
2026-05-08 13:51:21 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,19 +7,33 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/app-icon-192.png",
-  "./assets/app-icon-512.png"
+  "./assets/app-icon-512.png",
+  "./assets/beer.svg",
+  "./assets/wine.svg",
+  "./assets/vodka.svg",
+  "./assets/champagne.svg",
+  "./assets/cider.svg",
+  "./assets/cocktail.svg",
+  "./assets/whiskey.svg",
+  "./assets/liqueur.svg",
+  "./assets/other.svg"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
